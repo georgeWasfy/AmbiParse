c221bdcb3f48075480d3fdda5213619fa6329652
georgeWasfy <g.a.wasfy@gmail.com>
2024-08-23 20:50:06 +0300

    add first parser implementation

---
+++ tests/calculator.test.ts
@@ -0,0 +1,133 @@
+import { alt, apply, match, matchPattern, parse, seq } from "../src/parser";
+import * as assert from "assert";
+const altParser = alt();
+const seqParser = seq();
+const matchParser = match();
+const matchInParser = matchPattern();
+const applyParser = apply();
+
+const NUMBERS = matchInParser(`^\\d+(\\.\\d+)?`);
+const ADD = matchParser("+");
+const SUB = matchParser("-");
+const MUL = matchParser("*");
+const DIV = matchParser("/");
+const LPAREN = matchParser("(");
+const RPAREN = matchParser(")");
+
+function applyNumber(value: string): number {
+  return +value;
+}
+
+function applyFactor(value: string): string {
+  return value.replace("(", "").replace(")", "");
+}
+
+function applyBinary(value: string): number {
+  if (value.includes("+")) {
+    const result = value.split("+");
+    //@ts-ignore
+    if (result.length === 2) return +result[0] + +result[1];
+  }
+  if (value.includes("-")) {
+    const result = value.split("-");
+    //@ts-ignore
+    if (result.length === 2) return +result[0] - +result[1];
+  }
+  if (value.includes("*")) {
+    const result = value.split("*");
+    //@ts-ignore
+    if (result.length === 2) return +result[0] * +result[1];
+  }
+  if (value.includes("/")) {
+    const result = value.split("/");
+    //@ts-ignore
+    if (result.length === 2) return +result[0] / +result[1];
+  }
+  throw new Error(`Unknown binary operator`);
+}
+//  GRAMMAR
+// expr -> expr "+" term
+// | expr "-" term
+// | term
+
+// term -> term "*" factor
+// | term "/" factor
+// | factor
+
+// factor -> "(" expr ")"
+// | num
+
+const expr_parser = altParser(
+  () =>
+    applyParser(
+      () =>
+        seqParser(
+          () => expr_parser,
+          () => ADD,
+          () => term_parser
+        ),
+      applyBinary
+    ),
+  () =>
+    applyParser(
+      () =>
+        seqParser(
+          () => expr_parser,
+          () => SUB,
+          () => term_parser
+        ),
+      applyBinary
+    ),
+  () => term_parser
+);
+
+const term_parser = altParser(
+  () =>
+    applyParser(
+      () =>
+        seqParser(
+          () => term_parser,
+          () => MUL,
+          () => factor_parser
+        ),
+      applyBinary
+    ),
+  () =>
+    applyParser(
+      () =>
+        seqParser(
+          () => term_parser,
+          () => DIV,
+          () => factor_parser
+        ),
+      applyBinary
+    ),
+  () => factor_parser
+);
+
+const factor_parser = altParser(
+  () =>
+    applyParser(
+      () =>
+        seqParser(
+          () => LPAREN,
+          () => expr_parser,
+          () => RPAREN
+        ),
+      applyFactor
+    ),
+  () => applyParser(() => NUMBERS, applyNumber)
+);
+
+const calcParser = parse(expr_parser);
+
+test(`Parser: calculator`, () => {
+  assert.strictEqual(calcParser("1")[0].value, 1);
+  assert.strictEqual(calcParser("1.5")[0].value, 1.5);
+  assert.strictEqual(calcParser("1 + 2")[0].value, 3);
+  assert.strictEqual(calcParser("1 - 2")[0].value, -1);
+  assert.strictEqual(calcParser("1 * 2")[0].value, 2);
+  assert.strictEqual(calcParser("1 / 2")[0].value, 0.5);
+  assert.strictEqual(calcParser("1 + 2 * 3 + 4")[0].value, 11);
+  assert.strictEqual(calcParser("(1 + 2) * (3 + 4)")[0].value, 21);
+});

9194dfd07d1188e4ca76daf4e0d9e7bdf26ffb62
georgeWasfy <g.a.wasfy@gmail.com>
2025-04-13 14:08:24 +0200

    Feel: context definition

---
+++ tests/feel/formal/Context.ts
@@ -0,0 +1,59 @@
+import { alt, apply, lazy, seq } from "../../../src/parser";
+import { LBRACE, RBRACE } from "../Lexer";
+import { expression } from "./Expression";
+import { COLON, COMMA, Name, StringLiteral } from "./Lexer";
+import { Expression } from "./main";
+
+type Key = string;
+
+type ContextEntry = {
+  type: "ContextEntry";
+  key: Key;
+  value: Expression;
+};
+
+export type Context = {
+  type: "Context";
+  entries: ContextEntry[];
+};
+const key = alt(Name, StringLiteral);
+const contextEntry = apply(
+  seq(
+    key,
+    apply(COLON, () => {}),
+    lazy(() => expression)
+  ),
+  ([key, _, value]: [string, string, Expression]): ContextEntry => ({
+    type: "ContextEntry",
+    key,
+    value,
+  })
+);
+const contextEntries = alt(
+  contextEntry,
+  seq(
+    contextEntry,
+    apply(COMMA, () => {}),
+    lazy(() => contextEntries)
+  )
+);
+export const context = apply(
+  alt(
+    apply(
+      seq(
+        apply(LBRACE, () => {}),
+        apply(RBRACE, () => {})
+      ),
+      () => []
+    ),
+    seq(
+      apply(LBRACE, () => {}),
+      contextEntries,
+      apply(RBRACE, () => {})
+    )
+  ),
+  (entries: (ContextEntry | undefined)[]) => ({
+    type: "Context",
+    entries: entries.filter((x) => x !== undefined),
+  })
+);
